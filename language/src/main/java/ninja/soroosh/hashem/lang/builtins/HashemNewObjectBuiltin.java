4bc753ed16f31e2bfb0d9a74360f96bd422de503
soroosh <soroosh.sarabadani@gmail.com>
2019-12-19 22:45:17 +0100

    jadid function :)

---
+++ language/src/main/java/ninja/soroosh/hashem/lang/builtins/HashemNewObjectBuiltin.java
@@ -0,0 +1,40 @@
+package ninja.soroosh.hashem.lang.builtins;
+
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.CachedContext;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.instrumentation.AllocationReporter;
+import com.oracle.truffle.api.interop.ArityException;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.interop.UnsupportedTypeException;
+import com.oracle.truffle.api.library.CachedLibrary;
+import com.oracle.truffle.api.nodes.NodeInfo;
+import ninja.soroosh.hashem.lang.HashemLanguage;
+import ninja.soroosh.hashem.lang.runtime.HashemContext;
+import ninja.soroosh.hashem.lang.runtime.HashemNull;
+import ninja.soroosh.hashem.lang.runtime.HashemUndefinedNameException;
+
+/**
+ * Built-in function to create a new object. Objects in Hashemi are simply made up of name/value pairs.
+ */
+@NodeInfo(shortName = "jadid")
+public abstract class HashemNewObjectBuiltin extends HashemBuiltinNode {
+
+    @Specialization
+    @SuppressWarnings("unused")
+    public Object newObject(HashemNull o, @CachedContext(HashemLanguage.class) HashemContext context,
+                            @Cached("context.getAllocationReporter()") AllocationReporter reporter) {
+        return context.createObject(reporter);
+    }
+
+    @Specialization(guards = "!values.isNull(obj)", limit = "3")
+    public Object newObject(Object obj, @CachedLibrary("obj") InteropLibrary values) {
+        try {
+            return values.instantiate(obj);
+        } catch (UnsupportedTypeException | ArityException | UnsupportedMessageException e) {
+            /* Foreign access was not successful. */
+            throw HashemUndefinedNameException.undefinedFunction(this, obj);
+        }
+    }
+}
