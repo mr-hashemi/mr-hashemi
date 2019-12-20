b8fb69d83d2ca6b4bc0b485e74f5ba95136fb62e
soroosh <soroosh.sarabadani@gmail.com>
2019-12-20 13:42:21 +0100

    add native packager

---
+++ native/make_native.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -x
+if [[ $HASHEMI_BUILD_NATIVE == "false" ]]; then
+    echo "Skipping the native image build because HASHEMI_BUILD_NATIVE is set to false."
+    exit 0
+fi
+
+
+OPTS=""
+if [[ $(uname) != "Darwin" ]]; then
+  OPTS="--static"
+fi
+
+
+"$JAVA_HOME"/bin/native-image \
+    --macro:truffle --no-fallback --initialize-at-build-time \
+    -cp ../language/target/hashem.jar:../launcher/target/hashem-launcher.jar \
+    ninja.soroosh.hashem.lang.launcher.HashemiMain \
+    $OPTS hashem
+
