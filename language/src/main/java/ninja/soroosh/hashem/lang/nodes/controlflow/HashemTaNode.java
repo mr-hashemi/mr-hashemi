f640a62d356adf3aa7c3903e02b0a1f905924b5c
soroosh <soroosh.sarabadani@gmail.com>
2019-12-20 12:31:21 +0100

    ta node

---
+++ language/src/main/java/ninja/soroosh/hashem/lang/nodes/controlflow/HashemTaNode.java
@@ -0,0 +1,25 @@
+
+package ninja.soroosh.hashem.lang.nodes.controlflow;
+
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.LoopNode;
+import com.oracle.truffle.api.nodes.NodeInfo;
+import ninja.soroosh.hashem.lang.nodes.HashemStatementNode;
+import ninja.soroosh.hashem.lang.nodes.HashemExpressionNode;
+
+@NodeInfo(shortName = "ta", description = "The node implementing a ta loop")
+public final class HashemTaNode extends HashemStatementNode {
+
+    @Child private LoopNode loopNode;
+
+    public HashemTaNode(HashemExpressionNode conditionNode, HashemStatementNode bodyNode) {
+        this.loopNode = Truffle.getRuntime().createLoopNode(new HashemWhileRepeatingNode(conditionNode, bodyNode));
+    }
+
+    @Override
+    public void executeVoid(VirtualFrame frame) {
+        loopNode.execute(frame);
+    }
+
+}
