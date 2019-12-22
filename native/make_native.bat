ab95370878da37cde6f9b41da36dad70173c2533
soroosh <soroosh.sarabadani@gmail.com>
2019-12-22 10:48:17 +0100

    make_native win version

---
+++ native/make_native.bat
@@ -0,0 +1,2 @@
+"$JAVA_HOME"\bin\native-image --macro:truffle --no-fallback --initialize-at-build-time  -cp ../language/target/hashem.jar:../launcher/target/hashem-launcher.jar ninja.soroosh.hashem.lang.launcher.HashemiMain hashem-win
+
