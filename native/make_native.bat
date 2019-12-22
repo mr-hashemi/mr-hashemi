b72dc0e0eb19abab2bc111912af225ecd2c1b4d7
soroosh <soroosh.sarabadani@gmail.com>
2019-12-22 12:24:33 +0100

    release via batch files on windows

--- native/make_native.bat
+++ native/make_native.bat
@@ -1,3 +1,3 @@
-%JAVA_HOME%\bin\native-image --macro:truffle --no-fallback --initialize-at-build-time  -cp "..\language\target\hashem.jar;..\launcher\target\hashem-launcher.jar;" ninja.soroosh.hashem.lang.launcher.HashemiMain hashem-win.exe
+powershell %JAVA_HOME%\bin\native-image --macro:truffle --no-fallback --initialize-at-build-time  -cp "..\language\target\hashem.jar;..\launcher\target\hashem-launcher.jar;" ninja.soroosh.hashem.lang.launcher.HashemiMain hashem-win.exe
 
 
