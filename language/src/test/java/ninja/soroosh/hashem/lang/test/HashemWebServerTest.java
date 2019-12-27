5c5d7bf0dfc30840e14850de9483b31b804ef9a8
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2019-12-27 14:36:29 +0100

    json support workspace

--- language/src/test/java/ninja/soroosh/hashem/lang/test/HashemWebServerTest.java
+++ language/src/test/java/ninja/soroosh/hashem/lang/test/HashemWebServerTest.java
@@ -1,11 +1,24 @@
 package ninja.soroosh.hashem.lang.test;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.HostAccess;
 import org.graalvm.polyglot.Value;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
 
 //@Ignore
 public class HashemWebServerTest {
@@ -52,12 +65,14 @@ public class HashemWebServerTest {
     @Test
     public void factorialOf3() throws Exception {
         context = Context.newBuilder().allowCreateThread(true).allowAllAccess(true).out(System.out).build().create();
+        ObjectMapper mapper = new ObjectMapper();
+        final HashMap jsonNode = mapper.readValue("{\"person\":{\"name\":\"soroosh\"}}", JsonMap.class);
+
 
         // @formatter:off
         context.eval("hashemi",
                 "bebin server(request) {\n" +
-                        "  bechap(\"req is \"+ request.method);" +
-                        "  bechap(\"req is \"+ request.getMethod());" +
+                        "  bechap(\"req is \"+ request.get(\"person\"));" +
                         "  server1 = webserver(9091);" +
 //                        "  server2 = webserver(9092);" +
                         "  start(server1);" +
@@ -72,7 +87,7 @@ public class HashemWebServerTest {
         // @formatter:on
 
         server = context.getBindings("hashemi").getMember("server");
-        System.out.println(server.execute(new HttpReq()));
+        System.out.println(server.execute(jsonNode));
 
         context.close();
     }
@@ -92,5 +107,41 @@ public class HashemWebServerTest {
         @HostAccess.Export
         public String method = "POST";
     }
+
+    public static class JsonMapDeserializer extends StdDeserializer<JsonMap> {
+
+        public JsonMapDeserializer() {
+            this(null);
+        }
+
+        public JsonMapDeserializer(Class<?> vc) {
+            super(vc);
+        }
+
+        @Override
+        public JsonMap deserialize(JsonParser jp, DeserializationContext ctxt)
+                throws IOException, JsonProcessingException {
+            JsonNode node = jp.getCodec().readTree(jp);
+            if (node instanceof ObjectNode) {
+                ctxt.des
+                ((ObjectNode) node).elements();
+
+            }
+
+
+            return new JsonMap();
+        }
+    }
+
+    @JsonDeserialize(using = JsonMapDeserializer.class)
+    public static class JsonMap<K, V> extends HashMap<K, V> {
+        @HostAccess.Export
+        @Override
+        public V get(Object key) {
+            return super.get(key);
+        }
+    }
+
+
 }
 
